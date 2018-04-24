8b34917a1bf8636e2251247a032097976c21b3a5
Henrique <rick.eihara@gmail.com>
2018-04-23 23:44:52 -0300

    refact: create two pages, one to gather user's information, and another to choose the payment method

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,43 +1,13 @@
 <template>
   <div id="checkout">
-    <div class="row">
-      <div class="col-6">
-        Seus dados
-        <input @change="form_name_valid" type="text" id="name" placeholder="Nome" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-        <input @change="form_email_valid" type="text" id="email" placeholder="E-mail" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-        <input @change="form_cpf_valid" type="text" id="cpf" placeholder="CPF ex: 302.432.456-24" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-      </div>
+    <div class="col-12">
+      Seus dados
+      <input @change="form_name_valid" type="text" id="name" placeholder="Nome" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+      <input @change="form_email_valid" type="text" id="email" placeholder="E-mail" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+      <input @change="form_cpf_valid" type="text" id="cpf" placeholder="CPF ex: 302.432.456-24" class="form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
 
-      <div class="col-6">
-        Dados de Pagamento
-        <select @change="hide_select_form" class="custom-select mb-1" id="method_type">
-          <option selected>Forma de Pagamento</option>
-          <option value="Card">Cartão</option>
-          <option value="Boleto">Boleto</option>
-        </select>
-        <input @change="form_holder_name_valid" type="text" id="holder_name" placeholder="Nome Escrito" class="card-form form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-        <input @change="form_number_card_valid" type="text" id="number" placeholder="Número" class="card-form form-control mb-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-        <div class="input-group input-group-md mb-1">
-          <div class="input-group-prepend">
-            <span class="input-group-text card-form" id="inputGroup-sizing-md">Data de Expiração</span>
-          </div>
-          <input @change="form_expiration_date_valid" type="date" id="expiration_date" class="form-control card-form" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-        </div>
-        <input @change="form_cvv_valid" type="text" id="cvv" placeholder="CVV" class="form-control card-form" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-      </div>
+      <button @click="choose_payment_method" type="button" class="col-12 btn btn-success">Escolher pagamento</button>
     </div>
-
-  <div class="pt-3">
-    <b-button @click="show_modal" class="col-12 btn-success">
-      Realizar Pagamento
-    </b-button>
-    <b-modal ref="myModalRef" hide-footer title="Estado do Pagamento">
-      <div class="d-block text-center">
-        <h3>{{ product }}!</h3>
-      </div>
-      <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-btn>
-    </b-modal>
-  </div>
   </div>
 </template>
 
@@ -59,8 +29,6 @@ export default {
   },
   data() {
     return {
-      product: 'Boots',
-      status: '',
       payment: ''
     }
   },
@@ -97,152 +65,15 @@ export default {
         return false;
       }
     },
-    form_holder_name_valid () {
-      var holder_name_input = document.getElementById("holder_name");
-      if (holder_name_input.value.length > 4) {
-        holder_name_input.classList.remove("is-invalid");
-        return true;
-      } else {
-        holder_name_input.classList.add("is-invalid");
-      }
-        return false;
-    },
-    form_number_card_valid () {
-      var number_card_input = document.getElementById("number");
-      var re = /^\d{16}$/;
-      if (re.test(number_card_input.value.toLowerCase())) {
-        number_card_input.classList.remove("is-invalid");
-        return true;
-      } else {
-        number_card_input.classList.add("is-invalid");
-        return false;
-      }
-    },
-    form_expiration_date_valid () {
-      var expiration_date_input = document.getElementById("expiration_date");
-
-      var date_today = new Date();
-      var date_expiration = new Date(expiration_date_input.value);
-
-      if (date_today < date_expiration) {
-        expiration_date_input.classList.remove("is-invalid");
-        return true;
-      } else {
-        expiration_date_input.classList.add("is-invalid");
-        return false;
-      }
-    },
-    form_cvv_valid () {
-      var cvv_input = document.getElementById("cvv");
-      var re = /^\d{3}$/;
-      if (re.test(cvv_input.value.toLowerCase())) {
-        cvv_input.classList.remove("is-invalid");
-        return true;
-      } else {
-        cvv_input.classList.add("is-invalid");
-        return false;
-      }
-    },
-    show_modal () {
+    choose_payment_method () {
       if (this.validate_form()) {
-        this.$refs.myModalRef.show();
-        this.prepare_json();
-        this.send_request();
-      }
-    },
-    hide_select_form() {
-      var method_type = document.getElementById("method_type").value;
-      var card_forms = document.querySelectorAll(".card-form");
-      if (method_type == "Card") {
-        for (let card of card_forms) {
-          card.classList.remove("hidden-form");
-        }
-      } else {
-        for (let card of card_forms) {
-          card.classList.add("hidden-form");
-        }
+        // todo: need to store the data to the current session
+        window.location.replace("http://localhost:8080/checkout/payment");
       }
     },
     validate_form () {
-      var method_type = document.getElementById("method_type").value;
-
-      if (method_type == "Card") {
-        return this.form_name_valid() && this.form_email_valid() && this.form_cpf_valid() &&
-               this.form_email_valid() && this.form_holder_name_valid() &&
-               this.form_number_card_valid() && this.form_expiration_date_valid() &&
-               this.form_cvv_valid();
-      } else {
-        return this.form_name_valid() && this.form_email_valid() && this.form_cpf_valid() &&
-               this.form_email_valid();
-      }
+      return this.form_name_valid() && this.form_email_valid() && this.form_cpf_valid();
     },
-    hideModal () {
-      if (this.status === "success") {
-        window.location.replace("http://localhost:8080");
-      }
-      this.$refs.myModalRef.hide()
-    },
-    prepare_json() {
-      name = document.getElementById("name").value;
-      email = document.getElementById("email").value;
-      cpf = document.getElementById("cpf").value;
-      method_type = document.getElementById("method_type").value;
-
-      if (method_type === "Boleto") {
-        this.payment = { "payment": {
-                            method_type,
-                            "amount": "123.30",
-                            "client": {
-                              "client_id": "1"
-                            },
-                            "buyer": {
-                              name,
-                              email,
-                              "CPF": cpf
-                            }
-                          }
-                        };
-      } else if (method_type === "Card") {
-        holder_name = document.getElementById("holder_name").value;
-        number = document.getElementById("number").value;
-        expiration_date = document.getElementById("expiration_date").value;
-        cvv = document.getElementById("cvv").value;
-
-        this.payment = { "payment": {
-                            method_type,
-                            "amount": "123.30",
-                            "client": {
-                              "client_id": "1"
-                            },
-                            "buyer": {
-                              name,
-                              email,
-                              "CPF": cpf
-                            },
-                            "method": {
-                              holder_name,
-                              number,
-                              expiration_date,
-                              cvv
-                            }
-                          }
-                        };
-      }
-    },
-    send_request () {
-      console.log(this.payment);
-      this.$http.post('http://localhost:3000/api/v1/payments/', this.payment).then(response => {
-        if (response.body.status === "success") {
-          this.product = "Pagamento efetuado!";
-        } else {
-          this.product = "Pagamento não efetuado!";
-        }
-        this.status = response.body.status;
-        console.log(response);
-      }, response => {
-        console.log("ERROR" + response);
-      });
-    }
   },
   components: {
     FontAwesomeIcon
