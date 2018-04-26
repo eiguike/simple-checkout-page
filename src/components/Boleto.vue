48d0c8bf98a48e3af8e6d5f29383e341e5303ce0
Henrique <rick.eihara@gmail.com>
2018-04-26 00:58:20 -0300

    feat: create a boleto page, to view its number

---
+++ src/components/Boleto.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="boleto_page">
+    <div class="col-12">
+      {{ boleto_number }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'boleto_page',
+  data() {
+    return {
+      boleto_number: ''
+    }
+  },
+  mounted() {
+    var payments = JSON.parse(sessionStorage.getItem("payments"));
+    if (payments == null) {
+      this.boleto_number = "Boleto inválido"
+    } else {
+      this.boleto_number = "O número do seu boleto é: " + payments["number"];
+      sessionStorage.clear();
+    }
+  }
+}
+
+</script>
+
