0e97b16a69fffa91b7727c22d937d71a93c32d17
Henrique <rick.eihara@gmail.com>
2018-04-26 00:54:27 -0300

    feat/refact: create new routes for boleto and card payments, remove unnecessary lines, add new lines for jquery initialization

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import BootstrapVue from "bootstrap-vue"
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
-import App from './App.vue'
 import About from './components/About.vue'
 import Content from './components/Content.vue'
 import ShoppingCart from './components/ShoppingCart.vue'
+import App from './App.vue'
 import Checkout from './components/Checkout.vue'
 import Payment from './components/Payment.vue'
+import Boleto from './components/Boleto.vue'
+import Card from './components/Card.vue'
+import CardJs from 'card'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
@@ -20,7 +23,8 @@ Vue.use(VueResource)
 const routes = [
   { path: '/', component: Content },
   { path: '/checkout', component: Checkout },
-  { path: '/checkout/payment', component: Payment }
+  { path: '/checkout/boleto', component: Boleto },
+  { path: '/checkout/card', component: Card }
 ]
 
 const router = new VueRouter({
@@ -34,3 +38,8 @@ new Vue({
   components: { App },
   router
 })
+
+var $ = require("jquery");
+// The current card.js code does not explictly require jQuery, but instead uses the global, so this line is needed.
+window.jQuery = $;
+var card = require("card");
